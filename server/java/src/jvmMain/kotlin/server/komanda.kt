cdb8d11f14586182007a93c7bd5217d1fb310f33
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-05-29 12:43:24 -0300

    refactor(server:komanda): remove type aliases from commands

--- server/java/src/jvmMain/kotlin/server/komanda.kt
+++ server/java/src/jvmMain/kotlin/server/komanda.kt
@@ -22,12 +22,6 @@ import andesite.protocol.misc.Chat
 
 internal fun createKomandaRoot(): KomandaRoot {
   return KomandaRoot {
-    alias<Int>("int")
-    alias<Double>("double")
-    alias<Float>("float")
-    alias<Byte>("byte")
-    alias<Short>("short")
-
     onFailure { failure ->
       when (failure.cause) {
         null -> sendMessage(failure.chat)
