ea8aa585bc82c542cd5969350d78c46ebb4899f7
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-06-07 22:32:54 -0300

    build: remove kotlin native temporally due to https://youtrack.jetbrains.com/issue/KT-52677

--- andesite-komanda/build.gradle.kts
+++ andesite-komanda/build.gradle.kts
@@ -15,14 +15,15 @@
  */
 
 kotlin {
-  val hostOs = System.getProperty("os.name")
-  val isMingwX64 = hostOs.startsWith("Windows")
-  when {
-    hostOs == "Mac OS X" -> macosX64("native")
-    hostOs == "Linux" -> linuxX64("native")
-    isMingwX64 -> mingwX64("native")
-    else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-  }
+//  https://youtrack.jetbrains.com/issue/KT-52677
+//  val hostOs = System.getProperty("os.name")
+//  val isMingwX64 = hostOs.startsWith("Windows")
+//  when {
+//    hostOs == "Mac OS X" -> macosX64("native")
+//    hostOs == "Linux" -> linuxX64("native")
+//    isMingwX64 -> mingwX64("native")
+//    else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+//  }
 
   explicitApi()
 
