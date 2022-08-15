9d5a3918e85b7932c4f66ead8b64cb1a3ac36252
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-08-15 01:33:54 -0300

    fix(docs): point `editUrl` to project's url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -34,9 +34,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/gabrielleeg1/andesite/tree/main/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
